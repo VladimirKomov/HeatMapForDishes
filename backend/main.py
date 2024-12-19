62935f28a2ad75763b0ef8d815c348889451d90b
VladimirKomov <v.e.komov@gmail.com>
2024-12-19 10:35:51 +0200

    Add CORS and API integration between frontend and backend

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Settings CORS
+app.add_middleware(
+    CORSMiddleware,
+    # only local, [*] for all
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    # for methods
+    allow_methods=["*"],
+    # for headers
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
